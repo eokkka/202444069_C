ddbd484e02e023623e15a75c9be3f849f5d6c689
eokkka <118369043+eokkka@users.noreply.github.com>
2024-03-19 12:14:38 +0900

    Update test5.c

--- 20240319/test5.c
+++ 20240319/test5.c
@@ -12,6 +12,13 @@ int main(void)
      printf("%c",cnt);
     }
     
+
+    printf("\n알파벳 소문자가 출력됩니다. \n");
+
+    for(cnt= 97;cnt<=122;cnt++){
+     printf("%c",cnt);
+    }
+    
     return 0;
 
 
