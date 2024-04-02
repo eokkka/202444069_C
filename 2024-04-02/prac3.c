074adb0e8d84e0a1d51829b0c094b0bc2a092878
eokkka <118369043+eokkka@users.noreply.github.com>
2024-04-02 12:29:54 +0900

    Update prac3.c

--- 2024-04-02/prac3.c
+++ 2024-04-02/prac3.c
@@ -4,7 +4,6 @@
 int main(){
 
 int i=0;
-
 while(1){
 
     printf("1. 입력하기\n");
@@ -12,27 +11,21 @@ while(1){
     printf("3. 삭제하기\n");
     printf("4. 끝내기\n");
     printf("작업할 번호를 선택하세요. ");
+
     scanf("%d",&i);
 
-    switch(i){
-
-        case 1:
-             printf("\n입력하기를 선택하였습니다.\n");
-
-        case 2:
-             printf("\n출력하기를 선택하였습니다.\n");
-
-        case 3:
+        if(i==1)
+            printf("\n입력하기를 선택하였습니다.\n");
+        else if(i==2)
+            printf("\n출력하기를 선택하였습니다.\n");
+        else if(i==3)     
             printf("\n삭제하기를 선택하였습니다.\n");
-
-        case 4:
+        else if(i==4){
             printf("\n끝내기를 선택하였습니다.\n");
-            break;
-
-        default :
+            return 0;
+        }  
+        else
             printf("잘못 입력하였습니다.\n");
 
     }
-   
-}
 }
